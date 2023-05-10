0ed60f42cc3bdf0333013cfad835ebf8e2747986
TravisIPeterson <travisianpeterson@gmail.com>
2023-05-09 17:18:46 -0700

    Complete project (extra credit possibly to follow)

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -14,8 +14,7 @@ class LinkedList
             @head = new_node
             @tail = new_node
         else
-            current_node = @tail
-            current_node.next_node = new_node
+            @tail.next_node = new_node
             @tail = new_node
         end
     end
@@ -28,7 +27,7 @@ class LinkedList
     def size
         counter = 0
         current_node = @head
-        until @head.next_node.nil?
+        until current_node.next_node.nil?
             current_node = current_node.next_node
             counter += 1
         end
@@ -45,11 +44,74 @@ class LinkedList
         current_node.value
     end
 
+    def pop
+        if size == 1
+            @head = nil
+            @tail = nil
+        else
+            current_node = @head
+            until current_node.next_node == @tail
+                current_node = current_node.next_node
+            end
+            current_node.next_node = nil
+            @tail = current_node
+        end
+    end
+
+    def contains?(value)
+        current_node = @head
+        contains = false
+        until current_node.nil?
+            contains = true if current_node.value == value
+            current_node = current_node.next_node
+        end
+        contains
+    end
+
+    def find(value)
+        index = nil
+        current_index = 0
+        current_node = @head
+        until current_node.next_node == nil
+            index = current_index if current_node.value == value
+            current_index += 1
+            current_node = current_node.next_node
+        end
+        index
+    end
+
+    def to_s
+        current_node = @head
+        node_string = ""
+        until current_node.nil?
+            node_string += " ( #{current_node.value} ) -> "
+            current_node = current_node.next_node
+        end
+        node_string += "nil"
+        node_string
+    end
+
 end
 
 list = LinkedList.new
 list.append(3)
 list.append(4)
 list.prepend(17)
+list.append(5)
+list.append(1)
+list.append(8)
+p list.tail
+p list.size
 p list
-p list.at(1)
+list.pop
+puts '-------------------------------'
+p list.tail
+p list.size
+p list
+p list.head
+p list.tail
+puts list.contains?(17)
+puts list.contains?(56)
+p list.find(17)
+p list.find(56)
+puts list.to_s
